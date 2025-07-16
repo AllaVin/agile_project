29e13bb46f2d539826efa9ce9bb0d617af306f3d
matyashalla@gmail.com <matyashalla@gmail.com>
2025-07-16 10:19:19 +0200

    Task 6

--- apps/tasks/urls.py
+++ apps/tasks/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from apps.tasks.views.tag_views import *
 
 urlpatterns = [
-    path('tags/', TagListCreateAPIView.as_view(), name='tag-list-create'),
+    path('tags/', TagListCreateAPIView.as_view(), name='tag-list-create'), # http://127.0.0.1:8000/api/v1/tasks/tags/
+    path('tags/<int:pk>/', TagDetailUpdateDeleteAPIView.as_view(), name='tag-detail-update-delete'), # http://127.0.0.1:8000/api/v1/tasks/tags/1/
 ]
