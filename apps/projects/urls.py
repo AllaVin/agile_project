fea38f7e086cc878b09bd53b584d6f3e7297305d
matyashalla@gmail.com <matyashalla@gmail.com>
2025-07-23 10:25:04 +0200

    Practice 6. Task 4

--- apps/projects/urls.py
+++ apps/projects/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+
+from apps.projects.views.project_file_views import ListCreateProjectFileAPIView
 from apps.projects.views.project_views import *
 
 urlpatterns = [
@@ -7,4 +9,5 @@ urlpatterns = [
     # число (id проекта) и передать его в наш view как аргумент 'pk'.
     path('<int:pk>/', ProjectDetailUpdateDeleteAPIView.as_view(), name='project-detail-update-delete'),
     # api/v1/projects/1/
+    path ('files/', ListCreateProjectFileAPIView.as_view(), name='project-file-list-create'), # http://127.0.0.1:8000/api/v1/projects/files
 ]
